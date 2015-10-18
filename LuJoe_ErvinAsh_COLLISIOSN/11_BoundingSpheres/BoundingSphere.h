d90309d542d53a70b808ed715ee2b1a6133b795d
Ashley Ervin <aee9149@rit.edu>
2015-10-18 15:31:10 -0400

    finished project

---
+++ LuJoe_ErvinAsh_COLLISIOSN/11_BoundingSpheres/BoundingSphere.h
@@ -0,0 +1,37 @@
+#ifndef _BOUNDINGSPHERES_H_
+#define _BOUNDINGSPHERES_H_
+
+#include "RE\ReEngAppClass.h"
+#include <vector>
+#include <SFML\Graphics.hpp>
+
+using namespace ReEng;
+
+
+class BoundingSphere
+{
+	std::vector<vector3> vertexList;
+	vector3 position;
+	float radius;
+	vector3 center;
+
+public:
+
+	BoundingSphere(std::vector<vector3> list);
+
+	bool IsColliding(BoundingSphere* pOther);
+
+	void UpdootPosition(vector3 v3Input);
+
+	void calcRadius();
+	void calcCenter(vector3 pos);
+
+	vector3 getPosition();
+	vector3 getCenter();
+	float getRadius();
+
+private:
+
+};
+
+#endif //BOUDNINGSPHERE
